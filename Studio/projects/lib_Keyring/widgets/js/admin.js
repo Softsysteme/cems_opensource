894e14fe80bd73e4dfdf6b3fa55a5f0dfa7e8186
fabienb <fabienb@localhost>
2012-05-30 16:53:04 +0000

    inprogress #2847 - added application add and delete in admin widget

--- Studio/projects/lib_Keyring/widgets/js/admin.js
+++ Studio/projects/lib_Keyring/widgets/js/admin.js
@@ -3,17 +3,17 @@
  */
 console.log("Convertigo Keyring library - administration");
 
-var convertigoBase = window.location.pathname.substring(0, window.location.pathname.indexOf("/", 1));
+var $applicationsGrid;
+var $usersGrid;
 
 $(function() {
 	console.log("Document ready!");
 
 	jQuery("#applications-grid").jqGrid({
 		datatype: "local",
-	   	colNames:['ID','Nom'],
+	   	colNames:['Nom'],
 	   	colModel:[
-	   		{name:'id',index:'id', width:55},
-	   		{name:'name',index:'name asc, invdate', width:400}
+	   		{name:'name', index:'name asc', width:400}
 	   	],
 	   	rowNum:10,
 	   	rowList:[10,20,30],
@@ -36,101 +36,109 @@ $(function() {
 	    viewrecords: true,
 	    sortorder: "desc"
 	}).navGrid('#users-grid-pager',{edit:false,add:false,del:false});
-	
+
+	$applicationsGrid = $("#applications-grid");
+	$usersGrid = $("#users-grid");
+
 	$("button").button();
 	
+	// Applications
 	$("#button-applications").click(function() {
 		getApplications();
 	});
 
+	$("#applications-add").click(function() {
+		addApplication();
+	});
+
+	$("#applications-delete").click(function() {
+		deleteApplication();
+	});
+
+	// Users
 	$("#button-users").click(function() {
 		getUsers();
 	});
 
 });
 
+function addApplication() {
+	var newApp = $("#applications-add-application").val();
+	
+	if (newApp.length > 0) {
+		libKeyringCall(
+			"AddApplication",
+			"ctxApplications",
+			{ application: newApp },
+			function($data) {
+				var $application = $data.find("application");
+				var applicationID = $application.attr("id");
+				var applicationName = $application.attr("name");
+				$applicationsGrid.addRowData(applicationID, { name: applicationName });
+				
+				$applicationsGrid.sortGrid("name");
+			}
+		);		
+	}
+}
+
+function deleteApplication() {
+	var selectedRowId = $applicationsGrid.getGridParam('selrow');
+	
+	if (selectedRowId) {
+		var selectedRow = $applicationsGrid.getRowData(selectedRowId);
+		var deletedApp = selectedRow["name"];
+		
+		libKeyringCall(
+			"DeleteApplication",
+			"ctxApplications",
+			{ application: deletedApp },
+			function($data) {
+				$applicationsGrid.delRowData(selectedRowId);
+			}
+		);
+	}
+}
+
 function getApplications() {
 	$(".form").hide();
 	$("#applications").show();
-	$("#applications-wait").show();
-	$("#applications-content").hide();
 	
-	$.post(
-			convertigoBase + "/projects/lib_Keyring/.xml",
-			{
-				__sequence: "GetApplications",
-				__context: "ctxGetApplications"
-			},
-			function(data) {
-				$("#applications-wait").hide();
-				$("#applications-content").show();
-				
-				console.log("Applications data:");
-				console.log(data);
-				
-				var $data = $(data);
-				var $applicationsGrid = $("#applications-grid");
+	libKeyringCall(
+		"GetApplications",
+		"ctxApplications",
+		{ },
+		function($data) {
+			$applicationsGrid.clearGridData();
 
-				$applicationsGrid.clearGridData();
-
-				$data.find("application").each(function(index) {
-					var $application = $(this);
-					var applicationID = $application.attr("id");
-					var applicationName = $application.attr("name");
-					$applicationsGrid.addRowData(applicationID, { id: applicationID, name: applicationName });
-				});
-				
-			},
-			"xml"
-	)
-    .error(function(xhr, errorMessage, exception) {
-    	console.log("Error when getting applications list: " + errorMessage);
-    	console.log(exception.message);
-    	$("#users-wait").hide();
-    	$("#users-content").show();
-    });
+			$data.find("application").each(function(index) {
+				var $application = $(this);
+				var applicationID = $application.attr("id");
+				var applicationName = $application.attr("name");
+				$applicationsGrid.addRowData(applicationID, { id: applicationID, name: applicationName });
+			});
+		}
+	);		
 }
 
 function getUsers() {
 	$(".form").hide();
 	$("#users").show();
-	$("#users-wait").show();
-	$("#users-content").hide();
 	
-	$.post(
-			convertigoBase + "/projects/lib_Keyring/.xml",
-			{
-				__sequence: "GetUsersAndApplications",
-				__context: "ctxGetUsers"
-			},
-			function(data) {
-				$("#users-wait").hide();
-				$("#users-content").show();
-				
-				console.log("Users data:");
-				console.log(data);
-				
-				var $data = $(data);
-				var $usersGrid = $("#users-grid");
-				
-				$usersGrid.clearGridData();
-	
-				$data.find("user").each(function(index) {
-					var $user = $(this);
-					var userID = $user.attr("id");
-					var username = $user.attr("name");
-					var applications = $user.attr("applications");
-					$usersGrid.addRowData(userID, { id: userID, name: username, applications: applications });
-				});
-				
-			},
-			"xml"
-	)
-    .error(function(xhr, errorMessage, exception) {
-    	console.log("Error when getting users list: " + errorMessage);
-    	console.log(exception.message);
-    	$("#users-wait").hide();
-    	$("#users-content").show();
-    });
-
+	libKeyringCall(
+		"GetUsersAndApplications",
+		"ctxGetUsersAndApplications",
+		{ },
+		function($data) {
+			$usersGrid.clearGridData();
+			
+			$data.find("user").each(function(index) {
+				var $user = $(this);
+				var userID = $user.attr("id");
+				var username = $user.attr("name");
+				var applications = $user.attr("applications");
+				$usersGrid.addRowData(userID, { id: userID, name: username, applications: applications });
+			});
+		}
+	);		
 }
