d12c315420007e245af3a73d29b199cddeda32db
elodiee <elodiee@localhost>
2012-10-29 17:56:04 +0000

    INPROGRESS #2847
    updated keyring library project : 
     * updated some sequences again to add error management
     * admin widget updated with text translation to english, the new changes in sequences input/output, plus improved behavior
     * keychain widget updated only for text translation to english

--- Studio/projects/lib_Keyring/widgets/js/admin.js
+++ Studio/projects/lib_Keyring/widgets/js/admin.js
@@ -6,6 +6,7 @@ console.log("Convertigo Keyring library - administration");
 var $applicationsGrid;
 var $usersGrid;
 var applications = [];
+var errorMessages = new Array();
 
 var current_iRow;
 var current_iCol;
@@ -13,9 +14,16 @@ var current_iCol;
 $(function() {
 	console.log("Document ready!");
 
+	// retrieving column names for applications grid
+	var appColumns = [];
+	$("#app-col-template>p").each(function(index) {
+		appColumns[index] = $(this).text();
+	});
+	$("#app-col-template").hide();
+	// building applications grid
 	jQuery("#applications-grid").jqGrid({
 		datatype: "local",
-	   	colNames:['Nom'],
+	   	colNames: appColumns,
 	   	colModel:[
 	   		{name:'name', editable:true, edittype:'text', width:400}
 	   	],
@@ -32,9 +40,16 @@ $(function() {
 	    }
 	}).navGrid('#applications-grid-pager',{edit:false,add:false,del:false});
 	
+	// retrieving column names for users grid
+	var userColumns = [];
+	$("#user-col-template>p").each(function(index) {
+		userColumns[index] = $(this).text();
+	});
+	$("#user-col-template").hide();
+	// building users grid
 	jQuery("#users-grid").jqGrid({
 		datatype: "local",
-	   	colNames:['Nom','Applications'],
+	   	colNames: userColumns,
 	   	colModel:[
 	   		{name:'name', editable:true, edittype:'text', width:200},
 	   		{name:'applications', editable:false, width:400}
@@ -98,12 +113,22 @@ function addApplication(newApp) {
 			"default",
 			{ application: newApp },
 			function($data) {
-				var $application = $data.find("application");
-				var applicationID = $application.attr("id");
-				var applicationName = $application.attr("name");
-				$applicationsGrid.addRowData(applicationID, { name: applicationName });
-				
-				$applicationsGrid.sortGrid("name", true);
+				// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+				if (handleApplicativeErrors($data)) {
+					// error was found and displayed, nothing else to do
+				} else {
+				// no Convertigo exception nor status false (error code), handles sequence response
+					var $application = $data.find("application");
+					var applicationID = $application.attr("id");
+					var applicationName = $application.attr("name");
+					// adding row in the jqgrid
+					$applicationsGrid.addRowData(applicationID, { name: applicationName });
+					// sorting the jqgrid
+					$applicationsGrid.sortGrid("name", true);
+					
+					// emptying the add input
+					$("#applications-add-application").val("");
+				}
 			}
 		);		
 	}
@@ -114,7 +139,18 @@ function updateApplication(appID, appName) {
 		libKeyringCall(
 			"UpdateApplication",
 			"default",
-			{ applicationID: appID, name: appName }
+			{ applicationID: appID, name: appName }, 
+			function($data) {
+				// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+				if (handleApplicativeErrors($data)) {
+					// error was found and displayed, nothing else to do
+				} else {
+					// no Convertigo exception nor status false (error code), handles sequence response
+//					var $updatedApp = $data.find("updatedRows>application");
+//					var numberOfUpdatedApp = parseInt($updatedApp.text());
+					// nothing to do, update ok
+				}
+			}
 		);		
 	}
 }
@@ -124,15 +160,21 @@ function deleteApplication(selectedRowId) {
 		// Prevents currently edited cell to interfere with deletion process
 		$applicationsGrid.restoreCell(current_iRow, current_iCol);
 		
-		var selectedRow = $applicationsGrid.getRowData(selectedRowId);
-		var deletedApp = selectedRow["name"];
+//		var selectedRow = $applicationsGrid.getRowData(selectedRowId);
+//		var deletedApp = selectedRow["name"];
 		
 		libKeyringCall(
 			"DeleteApplication",
 			"default",
-			{ application: deletedApp },
+			{ applicationID: selectedRowId },
 			function($data) {
-				$applicationsGrid.delRowData(selectedRowId);
+				// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+				if (handleApplicativeErrors($data)) {
+					// error was found and displayed, nothing else to do
+				} else {
+				// no Convertigo exception nor status false (error code), handles sequence response
+					$applicationsGrid.delRowData(selectedRowId);
+				}
 			}
 		);
 	}
@@ -147,18 +189,24 @@ function getApplications() {
 		"default",
 		{ },
 		function($data) {
-			$applicationsGrid.clearGridData();
-			applications = [];
-
-			$data.find("application").each(function(index) {
-				var $application = $(this);
-				var applicationID = $application.attr("id");
-				var applicationName = $application.attr("name");
-				$applicationsGrid.addRowData(applicationID, { id: applicationID, name: applicationName });
-				applications.push(applicationName);
-			});
-
-			console.log("Applications: " + applications);
+			// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+			if (handleApplicativeErrors($data)) {
+				// error was found and displayed, nothing else to do
+			} else {
+			// no Convertigo exception nor status false (error code), handles sequence response
+				$applicationsGrid.clearGridData();
+				applications = [];
+	
+				$data.find("applications>application").each(function(index) {
+					var $application = $(this);
+					var applicationID = $application.attr("id");
+					var applicationName = $application.text();
+					$applicationsGrid.addRowData(applicationID, { id: applicationID, name: applicationName });
+					applications.push(applicationName);
+				});
+	
+				console.log("Applications: " + applications);
+			}
 		}
 	);		
 }
@@ -170,12 +218,22 @@ function addUser(newUser) {
 			"default",
 			{ user: newUser },
 			function($data) {
-				var $user = $data.find("user");
-				var userID = $user.attr("id");
-				var userName = $user.attr("name");
-				$usersGrid.addRowData(userID, { name: userName });
-				
-				$usersGrid.sortGrid("name");
+				// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+				if (handleApplicativeErrors($data)) {
+					// error was found and displayed, nothing else to do
+				} else {
+				// no Convertigo exception nor status false (error code), handles sequence response
+					var $user = $data.find("user");
+					var userID = $user.attr("id");
+					var userName = $user.attr("name");
+					// adding row in the jqgrid
+					$usersGrid.addRowData(userID, { name: userName });
+					// sorting the jqgrid
+					$usersGrid.sortGrid("name");
+					
+					// emptying the add input
+					$("#users-add-user").val("");
+				}
 			}
 		);		
 	}
@@ -186,7 +244,18 @@ function updateUser(userID, userName) {
 		libKeyringCall(
 			"UpdateUser",
 			"default",
-			{ userID: userID, name: userName }
+			{ userID: userID, name: userName }, 
+			function($data) {
+				// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+				if (handleApplicativeErrors($data)) {
+					// error was found and displayed, nothing else to do
+				} else {
+					// no Convertigo exception nor status false (error code), handles sequence response
+//					var $updatedUser = $data.find("updatedRows>user");
+//					var numberOfUpdatedUser = parseInt($updatedUser.text());
+					// nothing to do, update ok
+				}
+			}
 		);		
 	}
 }
@@ -196,15 +265,21 @@ function deleteUser(selectedRowId) {
 		// Prevents currently edited cell to interfere with deletion process
 		$usersGrid.restoreCell(current_iRow, current_iCol);
 		
-		var selectedRow = $usersGrid.getRowData(selectedRowId);
-		var deletedUser = selectedRow["name"];
+//		var selectedRow = $usersGrid.getRowData(selectedRowId);
+//		var deletedUser = selectedRow["name"];
 		
 		libKeyringCall(
 			"DeleteUser",
 			"default",
-			{ user: deletedUser },
+			{ userID: selectedRowId },
 			function($data) {
-				$usersGrid.delRowData(selectedRowId);
+				// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+				if (handleApplicativeErrors($data)) {
+					// error was found and displayed, nothing else to do
+				} else {
+				// no Convertigo exception nor status false (error code), handles sequence response
+					$usersGrid.delRowData(selectedRowId);
+				}
 			}
 		);
 	}
@@ -219,15 +294,29 @@ function getUsers() {
 		"default",
 		{ },
 		function($data) {
-			$usersGrid.clearGridData();
-			
-			$data.find("user").each(function(index) {
-				var $user = $(this);
-				var userID = $user.attr("id");
-				var username = $user.attr("name");
-				var applications = $user.attr("applications");
-				$usersGrid.addRowData(userID, { id: userID, name: username, applications: applications });
-			});
+			// Handles Convertigo exception in XML response or error code (status false) to automatically pop the error dialog
+			if (handleApplicativeErrors($data)) {
+				// error was found and displayed, nothing else to do
+			} else {
+			// no Convertigo exception nor status false (error code), handles sequence response
+				$usersGrid.clearGridData();
+				
+				$data.find("users>user").each(function(index) {
+					var $user = $(this);
+					var userID = $user.attr("id");
+					var username = $user.attr("name");
+					var applications = "";
+					$user.find("applications>application").each(function(index) {
+						var $app = $(this);
+						var appName = $app.text();
+						if (applications != "") {
+							applications += ", ";
+						}
+						applications += appName;
+					});
+					$usersGrid.addRowData(userID, { id: userID, name: username, applications: applications });
+				});
+			}
 		}
 	);		
 }
--- Studio/projects/lib_Keyring/widgets/js/common.js
+++ Studio/projects/lib_Keyring/widgets/js/common.js
@@ -31,6 +31,91 @@ function handleError(message, httpContent) {
 	alert(errorMessage);
 }
 
+/**
+* This function fills the error divs with message and exception, and shows the error div as a dialog
+* @param errorMessage : the error message
+* @param errorDetails : the error details (optional)
+* @param errorCode : the error code
+*/
+function displayException(message, errorMessage, exception) {
+	console.log(message);
+	console.log(errorMessage);
+	console.log(exception);
+
+	$("#dialog-error-message").text(message);
+	$("#dialog-error-sub-message").text(errorMessage);
+	$("#dialog-error-exception-message").text(exception);
+	
+	$("#dialog-error").dialog({
+		resizable: false,
+		modal: true,
+		buttons: [
+					{
+						text: "OK",
+						click: function() {
+							$(this).dialog("close");
+						}
+					}
+		]
+	});
+
+}
+
+/**
+* This function fills the error divs with message and details, and shows the error div as a dialog
+* @param errorMessage : the error message
+* @param errorDetails : the error details (optional)
+* @param errorCode : the error code
+*/
+function displayErrorMessage(errorMessage, errorDetails, errorCode) {
+	console.log(errorCode);
+	console.log(errorMessage);
+	console.log(errorDetails);
+	
+	$("#errorMessage").text(errorMessage);
+	$("#errorDetails").text(errorDetails);
+	$("#error-standard").dialog({
+		modal: true,
+		draggable: false,
+		resizable: false,
+		buttons: { 
+			"Ok": function() { 
+				$(this).dialog("close");
+			}
+		}
+	});
+}
+
+
+/**
+* Function that displays the error dialog if an error is found in $doc
+* @param $doc the document element of XML response from a sequence 
+* @return true if an error is found and displayed, false if no error
+*/
+function handleApplicativeErrors($doc) {
+	// if Convertigo error or applicative error returned by sequence
+	if (($doc.find(">error").length) || ($doc.find(">errorCode").length && $doc.find(">errorCode").text() != "0")) {
+		var errorCode = "";
+		var errorMessage = "";
+		var errorDetails = "";
+		
+		if ($doc.find(">error").length) {
+			// Convertigo exception error
+			displayException("Error while executing an action", $doc.find(">error>message").text(), $doc.find(">error>exception").text())
+		} else if ($doc.find(">errorCode").length && $doc.find(">errorCode").text() != "0") {
+			// error code returned
+			errorCode = parseInt($doc.find(">errorCode").text());
+			errorMessage = getErrorMessage(errorCode);
+			errorDetails = $doc.find(">errorDetails").text();
+			// display error div
+			displayErrorMessage(errorMessage, errorDetails, errorCode);
+		} 
+		return true;
+	} else {
+		return false;
+	}
+}
+
 function libKeyringCall(sequence, context, params, successAction) {
 	console.log("libKeyringCall: " + sequence + " on context '" + context + "'");
 	
