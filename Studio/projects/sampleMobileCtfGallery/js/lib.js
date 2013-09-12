5321252329441396c2f7189a3f05e05b44d41521
fabienb <fabienb@localhost>
2013-09-12 14:54:07 +0000

    inprogress #3599 - (6.3.x) - improved gallery framework - now automatically creates the sample source code (pretty printed and syntax colored) - we can now have several samples inside a given topic and a description for each sample

--- Studio/projects/sampleMobileCtfGallery/js/lib.js
+++ Studio/projects/sampleMobileCtfGallery/js/lib.js
@@ -2,3 +2,11 @@ function onAfterRendering_getTopic($doc, c8oData) {
 	// Render source code
 	SyntaxHighlighter.highlight();
 }
+
+function formatHtmlSourceCode(fragment) {
+	var source = fragment.innerHTML;
+	source = vkbeautify.xml(source);
+	source = source.replace(">", "&gt;").replace("<", "&lt;");
+	
+	return source;
+}
