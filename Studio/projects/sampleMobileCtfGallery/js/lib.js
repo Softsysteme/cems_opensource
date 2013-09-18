50630ca5286dc0c410c7448169440f13c0d47c4a
fabienb <fabienb@localhost>
2013-09-18 07:03:54 +0000

    inprogress #3620 - (6.3.x) fixed index.html and improved lib.js

--- Studio/projects/sampleMobileCtfGallery/js/lib.js
+++ Studio/projects/sampleMobileCtfGallery/js/lib.js
@@ -4,8 +4,15 @@ function onAfterRendering_getTopic($doc, c8oData) {
 }
 
 function formatHtmlSourceCode(fragment) {
-	var source = fragment.innerHTML;
+	var source = (new XMLSerializer()).serializeToString(fragment);
+	
+	// Remove unwanted <fragment> node
+	source = source.replace("<fragment>", "").replace("</fragment>", "");
+	
+	// Indent the code
 	source = vkbeautify.xml(source);
+	
+	// HTML-format the result
 	source = source.replace(">", "&gt;").replace("<", "&lt;");
 	
 	return source;
