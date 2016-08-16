2fd53daaac50962020a8439c32e1337d19795911
charlesg <charlesg@localhost>
2016-08-16 15:57:38 +0000

    inprogress #5031 DONE: *demo *call etc...

--- Studio/projects/template_Ionic2/ionic/app/pages/SearchForm/SearchForm.ts
+++ Studio/projects/template_Ionic2/ionic/app/pages/SearchForm/SearchForm.ts
@@ -1,19 +1,36 @@
 /**
  * Created by charlesg on 10/08/2016.
  */
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {C8o} from "../../providers/convertigo-service/convertigo-service";
+import {NavController} from 'ionic-angular';
+import {ListForm} from "../ListForm/ListForm";
 
 @Component({
     templateUrl: 'build/pages/SearchForm/SearchForm.html',
     providers : [C8o]
 })
 export class SearchForm {
-    constructor() {
 
+    private PostalCode : string;
+    private date : string;
+    constructor(private c8o : C8o, private nav: NavController) {
+        this.PostalCode = "Paris";
+        this.date = "16/08/2016";
     }
 
-    search(username: string){//, password: string){
+    search(){//, password: string){
+        this.c8o.callJson("sampleMobileSearchRoom.LoadList").then(data => {
+            console.log(data);
+            this.nav.push(ListForm, data);
+            /*if(data.logon == "true"){
+                //this.nav.push(SearchForm);
+            }
+            else{
 
+            }*/
+
+
+        });
     }
 }
